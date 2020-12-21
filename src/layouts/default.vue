7fd08a7794f1e562a32c0f85af4269a98bfb5904
ivgtr <gear.1112t.o@gmail.com>
2020-12-21 11:07:56 +0900

    :lipstick: fix: ちょっとデザイン調整

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="root relative overflow-hidden bg-gray-800">
     <h1 class="text-4xl text-white font-bold text-center mt-4">i2Color</h1>
+    <p class="text-white text-sm text-center">画像から特徴色を抽出し表示するサイト</p>
     <main class="main w-full min-h-full flex items-center justify-center">
       <nuxt />
     </main>
